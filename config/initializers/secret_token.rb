356a8281dff1a795b87eb876e65ccedb90f5780d
sushmitharaos <sushmitharaos@gmail.com>
2012-10-09 11:56:54 +0530

    here comes the first commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConnectVolunteer::Application.config.secret_token = '192f8d6e3f29d1fe522fdab73b725f49063c6a8b6983bf115e6e4c1b89fc857b7d17bbafd77d291437657207b99214ce7be1cbe932e27a465e85482e8af96142'
